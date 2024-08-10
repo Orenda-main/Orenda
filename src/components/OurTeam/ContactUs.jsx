5ce9ae669c061629913e3225b0fa92af639d125d
promise <uwagboepromise5@gmail.com>
2024-08-10 18:27:09 +0100

    Latest update

--- src/components/OurTeam/ContactUs.jsx
+++ src/components/OurTeam/ContactUs.jsx
@@ -7,7 +7,7 @@ const ContactUs = () => {
         <h2 className="heading ~text-lg/[2rem] mt-4 ~mb-4/6">
           Have any Question?
         </h2>
-        <Link to="/contact-us">
+        <Link to="/contact">
           <button className="flex items-center justify-center bg-transparent hover:bg-orenda-purple transition-colors text-orenda-purple hover:text-white border border-orenda-purple px-4 py-2 font-semibold rounded-[2.5rem] w-full ~max-w-[16.75rem]/[25rem] mx-auto text-[1.125rem]">
             Contact Us
           </button>
